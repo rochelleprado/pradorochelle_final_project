9de54f05c77c8bdb60766fce2187f6518619d7bd
UDD L301 PC-15 <taporcotl.686.stud@cdd.edu.ph>
2026-07-07 15:18:19 +0800

    initial commit

---
+++ resources/views/auth/two-factor-challenge.blade.php
@@ -0,0 +1,6 @@
+<form action="{{ route('two-factor.login') }}" method="post">
+    @csrf
+
+    <input type="text" name="code" id="code" placeholder="Authentication Code">
+    <x-primary-button>Verify</x-primary-button>
+</form>
